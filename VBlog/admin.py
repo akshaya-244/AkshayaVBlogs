4692766b4edbf5b1be283e8258ec8719bfb99473
akshaya-244 <66912695+akshaya-244@users.noreply.github.com>
2021-03-21 16:04:59 +0530

    Add files via upload

---
+++ VBlog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import Post,Categories
+
+admin.site.register(Post)
+admin.site.register(Categories)
+
+# Register your models here.
+# Register your models here.
